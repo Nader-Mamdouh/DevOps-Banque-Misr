8d645f074591a118bca80141e3e2b58f925345a2
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-03 22:22:38 +0300

    tf test

--- Kary-test-tf/main.tf
+++ Kary-test-tf/main.tf
@@ -2,22 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Data source to fetch the latest Ubuntu AMI ID
+data "aws_ami_ids" "ubuntu" {
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  }
+}
+
 module "vpc" {
-  source        = "./VPC_Module"
-  cidr_block    = "10.0.0.0/16"
-  vpc_name      = "MyVPC"
+  source         = "./VPC_Module"
+  cidr_block     = "10.0.0.0/16"
+  vpc_name       = "MyVPC"
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  azs           = ["us-east-1a", "us-east-1b"]
+  azs            = ["us-east-1a", "us-east-1b"]
 }
 
 module "security_group" {
-  source = "./SecurityGroup_Module"
+  source  = "./SecurityGroup_Module"
   sg_name = "EC2-SG"
   vpc_id  = module.vpc.vpc_id
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = data.aws_ami_ids.ubuntu.ids[0]  # Use the first AMI ID
   instance_type = "t2.micro"
   subnet_id     = module.vpc.public_subnets[0]
   security_groups = [module.security_group.security_group_id]
@@ -26,3 +36,4 @@ resource "aws_instance" "web" {
     Name = "MyWebServer"
   }
 }
+
