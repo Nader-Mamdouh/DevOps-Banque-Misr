9fead198dcca09bfb96e8a5c259e752e8e538c9d
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-05 17:08:00 +0300

    update TF modules Test Outputs

--- Test-tf-Modules/VPC_Module/outputs.tf
+++ Test-tf-Modules/VPC_Module/outputs.tf
@@ -1,10 +1,10 @@
-# output "vpc_id" {
-#   value = aws_vpc.Macarious_VPC.id
-# }
+output "vpc_id" {
+  value = aws_vpc.Macarious_VPC.id
+}
 
-# output "public_subnets" {
-#   value = aws_subnet.public[*].id
-# }
-# output "private_subnets" {
-#   value = aws_subnet.private[*].id
-# }
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
