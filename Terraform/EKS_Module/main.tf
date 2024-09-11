ddcbbcfc5dacf5163aaf3a03c44e1c3b3efa4223
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 20:51:49 +0300

    update Terraform Modules

--- Terraform/EKS_Module/main.tf
+++ Terraform/EKS_Module/main.tf
@@ -14,21 +14,6 @@ resource "aws_iam_role" "Macarious_eks_cluster_role" {
   })
 }
 
-# IAM Role for EKS Worker Nodes
-resource "aws_iam_role" "Macarious_eks_worker_role" {
-  name = "Makarious-eks-worker-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
 # IAM Policy Attachment for EKS Cluster Role
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -40,23 +25,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceContr
   role       = aws_iam_role.Macarious_eks_cluster_role.name
 }
 
-# IAM Policy Attachment for EKS Worker Node Role
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.Macarious_eks_worker_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.Macarious_eks_worker_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.Macarious_eks_worker_role.name
-}
-
-# -------------------------------------------------------------- Creating Cluster, NodeGroup
+# -------------------------------------------------------------- Creating Cluster
 # Create the EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
@@ -73,31 +42,6 @@ resource "aws_eks_cluster" "eks_cluster" {
     Name = "eks-cluster"
   }
 }
-
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = var.node_group_name
-  node_role_arn   = aws_iam_role.Macarious_eks_worker_role.arn
-  subnet_ids      = var.private_subnet_ids
-
-  scaling_config {
-    desired_size = var.desired_size
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
-
-  instance_types = var.instance_types
-  tags = {
-    Name = "Macarious-eks-node-group"
-  }
-}
-
-# Add-ons for EKS Cluster
-# resource "aws_eks_addon" "coredns" {
-#   cluster_name = aws_eks_cluster.eks_cluster.name
-#   addon_name   = "coredns"
-#   addon_version = "v1.11.1-eksbuild.8"
-# }
 
 resource "aws_eks_addon" "eks_pod_identity_agent" {
   cluster_name = aws_eks_cluster.eks_cluster.name
--- Terraform/EKS_Module/variables.tf
+++ Terraform/EKS_Module/variables.tf
@@ -3,47 +3,22 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "sg_id" {
-  description = "ID of the security group to associate with the EKS cluster"
-  type        = string
-}
-
 variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  type        = string
-}
-
-variable "node_group_name" {
-  description = "The name of the EKS node group"
+  description = "The name of the EKS cluster."
   type        = string
 }
 
 variable "private_subnet_ids" {
-  description = "List of private subnet IDs where the EKS nodes will be deployed"
+  description = "List of private subnet IDs for the EKS cluster."
   type        = list(string)
 }
 
-variable "instance_types" {
-  description = "List of instance types for the EKS node group"
-  type        = list(string)
-}
-
-variable "desired_size" {
-  description = "Desired number of worker nodes"
-  type        = number
-}
-
-variable "max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-}
-
-variable "min_size" {
-  description = "Minimum number of worker nodes"
-  type        = number
+variable "sg_id" {
+  description = "The ID of the security group for the EKS cluster."
+  type        = string
 }
 
 variable "kubernetes_version" {
-  description = "Kubernetes version to use for the EKS cluster"
+  description = "The Kubernetes version for the EKS cluster."
   type        = string
 }
