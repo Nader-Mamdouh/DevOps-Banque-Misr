e39699133e9f0b0d8a89078de386e64b9bfd6812
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-12 16:36:21 +0300

    add outputs.tf in EC2 Modules

--- Terraform/EC2_Module/main.tf
+++ Terraform/EC2_Module/main.tf
@@ -1,3 +1,5 @@
+# This block defines an AWS EC2 instance resource named "ec2_instance"
+
 resource "aws_instance" "ec2_instance" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
@@ -8,14 +10,4 @@ resource "aws_instance" "ec2_instance" {
   tags = {
     Name = var.instance_name
   }
-}
-
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.ec2_instance.id
-}
-
-output "ec2_instance_public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.ec2_instance.public_ip
 }
