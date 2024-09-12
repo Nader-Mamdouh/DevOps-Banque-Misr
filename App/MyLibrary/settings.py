bb92a902764bd8cf12236fadd144f2fcb692d200
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-12 04:14:13 +0300

    upgrade K8s Readme

--- App/MyLibrary/settings.py
+++ App/MyLibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-691py28fdo&!00j)s=mue=7%jv*_*0$3v+_l^i=4x1j7f9!yq$'
+SECRET_KEY = 'django-insecure-691py28fdo&!00j)s=mue=7%jv*_*0$3v+_l^i=4x1j7f9!yq$'
 
 import os
 # import hvac
